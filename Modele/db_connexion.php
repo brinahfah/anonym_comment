a3cc1726817e945811667aa51f62c7e4b545654a
brinahfah <ebikie4@gmail.com>
2026-04-06 21:44:57 +0200

    main

--- Modele/db_connexion.php
+++ Modele/db_connexion.php
@@ -1,14 +1,15 @@
 <?php
-
 require_once __DIR__ . '/../vendor/autoload.php';
 
+// Charger les variables d'environnement (dotenv)
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
 $dotenv->load();
 
 try {
-
+    // Construire le DSN PostgreSQL
     $dsn = "pgsql:host={$_ENV['DB_HOST']};port={$_ENV['DB_PORT']};dbname={$_ENV['DB_NAME']};sslmode=require";
 
+    // Connexion PDO
     $pdo = new PDO(
         $dsn,
         $_ENV['DB_USER'],
@@ -20,7 +21,5 @@ try {
     return $pdo;
 
 } catch (PDOException $e) {
-
     die("Erreur de connexion : " . $e->getMessage());
-
 }
